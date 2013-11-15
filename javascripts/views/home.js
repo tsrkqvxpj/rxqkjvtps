aab59b0edbd6a4c9b29df037075655969bb12a81
kxqrstjpv <rtqskvpxj@gmail.com>
2013-11-14 23:37:42 -0800

    layout and started autocomplete

--- javascripts/views/home.js
+++ javascripts/views/home.js
@@ -1,8 +1,39 @@
+var Plugin = Backbone.Model.extend({
+    label: function () {
+        return this.get("name");
+    }
+});
+
+var PluginCollection = Backbone.Collection.extend({
+    model: Plugin
+});
+
+var c1 = new Plugin({name : "San Francisco"});
+var c2 = new Plugin({name : "Barcelona"});
+var c3 = new Plugin({name : "Seoul"});
+var c4 = new Plugin({name : "Beijing"});
+var c5 = new Plugin({name : "New Delhi"});
+var c6 = new Plugin({name : "Varanasi"});
+var c7 = new Plugin({name : "Rio de Janeiro"});
+var col = new PluginCollection([c1, c2, c3, c4, c5, c6, c7]);
+
+
+
+
 var HomeView = Backbone.View.extend({
 	el: '#main',
 
 	render: function() {
 		var template = _.template($('#template_home').html());
+		
+
 		this.$el.html(template);
+
+		new AutoCompleteView({
+		    input: this.$("#input_search"),
+		    model: col
+		}).render();
+
 	}
 });
+
